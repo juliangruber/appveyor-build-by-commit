ff1a7565fe4d707a48dfff16a05e024ccdaa415a
Julian Gruber <julian@juliangruber.com>
2017-06-07 18:01:10 +0200

    standard

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const got = require('got')
 module.exports = ({ repo, sha }) => {
   const getBuilds = () => {
     const url = `https://ci.appveyor.com/api/projects/${repo[0]}/${repo[1]}/history?recordsNumber=50`
-    return got(url, { json: true })
-      .then(res => res.body.builds)
+    return got(url, { json: true }).then(res => res.body.builds)
   }
 
   const resolveBuild = build => {
-   const url = `https://ci.appveyor.com/api/projects/${repo[0]}/${repo[1]}/build/${build.version}`
-   return got(url, { json: true })
-     .then(res => res.body.build)
+    const url = `https://ci.appveyor.com/api/projects/${repo[0]}/${repo[1]}/build/${build.version}`
+    return got(url, { json: true }).then(res => res.body.build)
   }
 
   return getBuilds()
